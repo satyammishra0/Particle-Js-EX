0781aa9bb8e667876a11d9c5883d5fb0e16ec077
satyam mishra <satyamjay007@gmail.com>
2023-07-04 08:59:01 +0530

    new smooth scroll project added

---
+++ smooth-scroll/app.js
@@ -0,0 +1,23 @@
+// Target -- where we want to take our page on click ||| target place target div
+// Duaration --  Duration we want that how much time it should take in scrolling
+
+function smoothScroll(target, duration) {
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startingPosition = window.pageYOffset;
+  var distance = targetPosition - startingPosition;
+  var startTime = null;
+
+  console.log(targetPosition);
+  console.log(startingPosition);
+  function animationScroll(currentTime) {
+    if (startTime == null) {
+      startTime = currentTime;
+    }
+    var timeElapsed = currentTime - startTime;
+  }
+
+  requestAnimationFrame(animationScroll());
+}
+
+smoothScroll(".section-2", 1000);
