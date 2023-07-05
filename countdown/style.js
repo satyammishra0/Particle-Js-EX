28c95c935fa7bd038a5dfffea91e5159552d82d5
satyam mishra <satyamjay007@gmail.com>
2023-07-05 16:09:17 +0530

    reverse string two forms done

--- countdown/style.js
+++ countdown/style.js
@@ -1,14 +1,32 @@
-var isMobile = window.matchMedia("(max-width: 768px)");
+// var isMobile = window.matchMedia("(max-width: 768px)");
 
-console.log(isMobile);
-function handleViewportChange(mobileView) {
-  if (mobileView.matches) {
-  } else {
-  }
-}
+// function handleViewportChange(isMobile) {
+//   return isMobile.matches;
+// }
 
 // Call the function initially to apply the appropriate changes
-handleViewportChange(isMobile);
+// console.log(handleViewportChange(isMobile));
 
 // Add an event listener to handle changes in viewport width
-isMobile.addListener(handleViewportChange);
+// isMobile.addListener(handleViewportChange);
+
+// 1st method of reverse string
+const string = "Hello, World!";
+function reverseString(string) {
+  return string.split("").reverse().join("");
+}
+
+console.log(reverseString(string));
+
+// 2nd method of reverse the string
+
+function reverseString2(str) {
+  let reverse = "";
+
+  for (const c of str) {
+    reverse = c + reverse;
+  }
+
+  return reverse;
+}
+
