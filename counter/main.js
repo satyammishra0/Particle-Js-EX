4e86ace95fa2cb2f5426ed3dae6cc2118968017b
satyam mishra <satyamjay007@gmail.com>
2023-07-10 12:10:36 +0530

    new increament and decreament project added

---
+++ counter/main.js
@@ -0,0 +1,40 @@
+// let minus = document.getElementById("minus");
+// let plus = document.getElementById("plus");
+// let number = document.getElementById("number");
+// let value = parseInt(number.value);
+
+// function decrease() {
+//   if (value > 0) {
+//     value--;
+//     number.value = value;
+//   } else {
+//     value = value;
+//     number.value = value;
+//   }
+// }
+
+// function increase() {
+//   value++;
+//   number.value = value;
+// }
+
+// minus.addEventListener("click", decrease);
+// plus.addEventListener("click", increase);
+
+const counter = document.getElementById("counter");
+
+counter.addEventListener("click", (event) => {
+  console.log(event.target);
+  const button = event.target;
+  const action = button.dataset.action;
+  const input = counter.querySelector("input");
+  let value = parseInt(input.value);
+
+  if (action === "decrease") {
+    value = Math.max(0, value - 1);
+  } else if (action === "increase") {
+    value++;
+  }
+
+  input.value = value;
+});
